01a204cb1d8e9be674130fd9eb5defdf05b92e56
Muhammad Fiqih Arahman <muhammadfiqih910@gmail.com>
2024-05-26 16:58:41 +0700

    add convert binary

--- routes.js
+++ routes.js
@@ -17,7 +17,43 @@ const routes = [
             response.code(200);
             return response;
         }
-    }
+    },
+    {
+        path: '/convert-binary',
+        method: 'POST',
+        handler: (request, h) => {
+
+            const { data } = request.payload;
+            let resultBinary = '';
+
+            if (typeof data !== 'number' && !Number.isInteger(data)) {
+                const response = h.response({
+                    status: 'fail',
+                    message: 'Input harus angka dan bilangan bulat!',
+                });
+                response.code(400);
+                return response;
+            }
+
+            if (data === 0) {
+                resultBinary = '0';
+            }
+
+            else {
+                while(data > 0) {
+                    resultBinary = (data % 2) + resultBinary;
+                    data = Math.floor(data / 2);
+                }
+            }
+
+            const response = h.response({
+                status: 'success',
+                result: resultBinary,
+            });
+            response.code(200);
+            return response;
+        }
+    },
 ];
 
 module.exports = routes;
